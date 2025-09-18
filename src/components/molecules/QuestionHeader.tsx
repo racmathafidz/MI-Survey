eb6a3d86074fc209f7385e6dbbc3702da5c89b11
racmathafidz <rac.hafidz@gmail.com>
2025-09-18 13:35:41 +0700

    Update: Adjust styling for timeLeft display in QuestionHeader component

--- src/components/molecules/QuestionHeader.tsx
+++ src/components/molecules/QuestionHeader.tsx
@@ -7,7 +7,7 @@ export default function QuestionHeader({ number, timeLeft }: Props) {
   return (
     <div className="d-flex justify-content-between align-items-center mb-3">
       <h2 className="text-secondary fw-bold mb-0">Q{number}</h2>
-      {timeLeft && <p>{timeLeft}</p>}
+      {timeLeft && <p className="mb-0 mt-0 align-self-center">{timeLeft}</p>}
     </div>
   );
 }
